a4b231aad85d9faaa5ad2ddc889166966ab92f92
masatoshiMoto <k11000660@gmail.com>
2019-05-30 21:45:03 +0900

    Lat Lon Read

--- GPS/gps.py
+++ GPS/gps.py
@@ -4,7 +4,6 @@ import difflib
 import pigpio
 
 RX=26
-i = 0
 
 try:
 	pi = pigpio.pi()
@@ -18,34 +17,40 @@ try:
 		(count, data) = pi.bb_serial_read(RX)
 		#print("b ", end="")
 		if count:
-
-			#print("c ", end="")
-
 			gpsData = data.decode('utf-8', 'replace')
-			#print("d ", end="")
 			#print(gpsData)
 			
-			#gga = gpsData.find('$GPRMC,')
+			gga = gpsData.find('$GPRMC,')
 			rmc = gpsData.find('$GPRMC,')
 			#gsa = gpsData.find('$GPGSA,')
 			#gsv = gpsData.find('$GPGSV,')
 			#vtg = gpsData.find('$GPVTGM')
-			#print("e ", end="")
-			if(gpsData[rmc:rmc+20].find("A") == -1): 
+			if(gpsData[rmc:rmc+20].find("V") != -1):	#Checking GPS Status
+				#Status V
 				print("Status V ", end="")
-				i = i + 1
 				print()
-				
-			else:
+			elif(gpsData[rmc:rmc+20].find("A") != -1):
+				#Status A
 				gprmc = gpsData[rmc+7:]
 				utctime = gpsData[rmc+7:rmc+17]
-				lat = gpsData[rmc+20:rmc+30]
-				lon = gpsData[rmc+32:rmc+43]
-				print(str(utctime) + " ", end="")
-				print(str(lat) + " ", end="")
-				print(str(lon) + " ")
-						
-		#print("f ")	
+				lat = gpsData[rmc+20:rmc+29]
+				lon = gpsData[rmc+32:rmc+42]
+				Lat = round(float(lat[:2]) + float(lat[2:]) / 60.0, 6)
+				Lon = round(float(lon[:3]) + float(lon[3:]) / 60.0, 6)
+				print("Time:" + str(utctime) + " ", end="")
+				print("Lat:" + str(Lat) + " ", end="")
+				print("Lon:" + str(Lon) + " ")#, end="")
+
+				gpgga = gpsData[gga:gga+60]
+				hight = gpgga.find(",M,")
+				#sHight = float(gpgga[hight-2:hight-1])
+				#gHight = float(gpgga[hight+4:hight+4])
+				#print("sHight:" + str(sHight) + " ")
+				#print("gHight:" + str(gHight) + " ")
+			else:
+				#No Status Data
+				print("Reading GPS Error")
+	
 		time.sleep(1)
 except KeyboardInterrupt:
 	pass
