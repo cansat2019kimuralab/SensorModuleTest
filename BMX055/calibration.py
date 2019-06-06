4f951959fcfff446d2ceff12db5d50eedd3f8c6f
masatoshi_moto <45708319+masatoshiMoto@users.noreply.github.com>
2019-06-06 14:24:17 +0900

    calibration done

--- BMX055/calibration.py
+++ BMX055/calibration.py
@@ -5,61 +5,60 @@ from matplotlib.patches import Ellipse
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
-def f(B, x):
+def ellipse(B, x):
     return ((x[0]/B[0])**2+(x[1]/B[1])**2-1.)
 
-#Least squares method with scipy.optimize
-def fit_func(parameter,xy):
-    a = parameter[0]
-    b = parameter[1]
-    c = parameter[2]
-    residual = (a*xy[0]+b*xy[1]+c)
-    return residual
-
-def g(B, x):
-    return ((x[0]/B[0])**2+(x[1]/B[1])**2-1.)
-
-if __name__ == '__main__':
+def calibration(path):
     #f = open("bmx055test.txt", "r")
-    f  = open("randomC.txt", "r")
-    lines = f.readlines()
-    x_csv = []
-    y_csv = []
-    xx_csv = x_csv
-    yy_csv = y_csv
-    for line in lines:
-        word = line.split()
-        x_csv.append(float(word[0]))
-        y_csv.append(float(word[1]))
 
+    with open(path, "r") as f:
+        lines = f.readlines()
+        x_csv = []
+        y_csv = []
+        for line in lines:
+            word = line.split()
+            x_csv.append(float(word[0]))
+            y_csv.append(float(word[1]))
+
+    '''
     for i in range(len(x_csv)):
         print(str(x_csv[i]) + " " + str(y_csv[i]))
     print()
-    f.close()
+    '''
 
+    xx_csv = x_csv
+    yy_csv = y_csv
     # sub average
     x_ave=np.average(x_csv)
     y_ave=np.average(y_csv)
+
     x_csv = x_csv-x_ave
     y_csv = y_csv-y_ave
 
     xy = np.array([x_csv, y_csv])
 
-    #mdr = odr.Model(f, implicit=True)
-    mdr = odr.Model(g, implicit=True)
+    mdr = odr.Model(ellipse, implicit=True)
     mydata = odr.Data(xy,y=1)
-    #myodr = odr.ODR(mydata, mdr, beta0=[1., 2.])
-    myodr = odr.ODR(mydata, mdr, beta0=[1., 2.,3.,4.])
+    myodr = odr.ODR(mydata, mdr, beta0=[1.,2.])
     myoutput = myodr.run()
-    myoutput.pprint()
+
+    """from ellipse to Circle"""
+    x_csv = x_csv / 2.*myoutput.beta[1]
+    y_csv = y_csv / 2.*myoutput.beta[0]
+
+    cal_data = [x_ave, y_ave, 20.*myoutput.beta[1], 20.*myoutput.beta[0]]
 
     ax = plt.subplot(111, aspect='equal')
-    plt.axes().set_aspect('equal', 'datalim')
-    plt.scatter(x_csv, y_csv, c="red")
     plt.scatter(xx_csv, yy_csv, c="blue")
-    ell = Ellipse(xy=(0., 0.), width=2.*myoutput.beta[0], height=2.*myoutput.beta[1], angle=0.0)
-    ell.set_facecolor('none')
-    ell.set_edgecolor('black')
-    ax.add_artist(ell) # fitted curve
+    plt.scatter(x_csv, y_csv, c="red")
     plt.grid()
     plt.show()
+
+    return cal_data
+
+
+if __name__ == '__main__':
+    file = 'randomC.txt'
+    cal_data = calibration(file)
+    for i in range(4):
+        print(cal_data[i])
--- BMX055/randomC.py
+++ BMX055/randomC.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     yy = []
     with open("randomC.txt", mode="w") as f:
         for i in range(100):
-            r = random.uniform(9.9, 10.1)
+            r = random.uniform(9.999, 10.001)
             theta = random.uniform(0, 2*math.pi)
-            x = 7.0 *r * math.cos(theta) + 11.0
-            y = 1.0 * r * math.sin(theta) + 4.0
+            x = 20.0 * r * math.cos(theta) + 70.0
+            y = 10.0 * r * math.sin(theta) - 30.0
             f.write("%f %f %f" % (x, y, 0.0))
             f.write("\n")
             xx.append(x)
