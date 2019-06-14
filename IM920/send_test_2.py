99a65edd24e3eae9db56246316fd862e8f0d6b8b
Pasises <45708319+Pasises@users.noreply.github.com>
2019-06-15 01:05:21 +0900

    Restored

--- IM920/send_test_2.py
+++ IM920/send_test_2.py
@@ -1,15 +1,12 @@
-# -*- coding: UTF-8 -*-
 import serial
 import binascii
 import signal
 import sys
 import time
 
-
 com = serial.Serial('/dev/ttyS0',19200)
 
 com.flushInput()
-
 com.write(b'TXDA ABCD'+ b'\r\n')
 com.readline()
 com.write(b'RDID' + b'\r\n')
