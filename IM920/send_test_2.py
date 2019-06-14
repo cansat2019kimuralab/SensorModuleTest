becbdc7fecef533169497ff9fc98fd67bdca5ee4
Pasises <45708319+Pasises@users.noreply.github.com>
2019-06-15 01:03:12 +0900

    Restored

---
+++ IM920/send_test_2.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+import serial
+import binascii
+import signal
+import sys
+import time
+
+
+com = serial.Serial('/dev/ttyS0',19200)
+
+com.flushInput()
+
+com.write(b'TXDA ABCD'+ b'\r\n')
+com.readline()
+com.write(b'RDID' + b'\r\n')
+com.flushOutput()
+print('固有ID:' + str(com.readline().strip()))
+com.close()
