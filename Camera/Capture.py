850163978718a32f24cfcbc3082ba22b1913add3
masatoshiMoto <k11000660@gmail.com>
2019-06-17 15:39:21 +0900

    ver3

--- Camera/Capture.py
+++ Camera/Capture.py
@@ -5,7 +5,10 @@ def CAPTURE(count):
     with picamera.PiCamera() as camera:
         camera.hflip = True
         camera.vflip = True
-        camera.resoltion = (640,480)
+        camera.resolution = (640,480)
         camera.start_preview()
         time.sleep(2)
-        camera.capture('photo'+'str(count)'+'.jpg')
+        camera.capture('photo'+str(count)+'.jpg')
+
+if __name__ == "__main__":
+	CAPTURE(1)
