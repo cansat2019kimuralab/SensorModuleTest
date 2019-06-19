5ec4a55adc2bdf55c5296b5b9bc698d447cfb279
s-arai14 <50451173+s-arai14@users.noreply.github.com>
2019-06-19 12:47:55 +0900

    Update convertIMG2BYTES.py

--- IM920/convertIMG2BYTES.py
+++ IM920/convertIMG2BYTES.py
@@ -2,18 +2,24 @@ import numpy as np
 import cv2
 from PIL import Image
 
-img = cv2.imread("target15.jpg",0) #cv2.imreadで画像ファイルを開いてnumpy.ndarrayを取得
+def IMGtoBYTES(img):
+    img_string = img.tobytes()
+    #print (img_string)
+    return img_string
 
-img_string = img.tobytes()
+def BYTEStoIMG(img_string):
+    img_array = np.fromstring(img_string,dtype ='uint8') #バイトデータ→ndarray変換
+    img_array = np.reshape(img_array,(240,320))
 
-print (img_string)
+    #dec_img = cv2.imdecode(img_array, 0)
+    pil_img = Image.fromarray(img_array)
 
-img_array = np.fromstring(img_string,dtype ='uint8') #バイトデータ→ndarray変換
-img_array = np.reshape(img_array,(240,320))
+    #cv2.imshow("decoded_image", dec_img)
+    #pil_img.show()
+    pil_img.save("decoded_target15.jpg")
+    return pil_img
 
-#dec_img = cv2.imdecode(img_array, 0)
-pil_img = Image.fromarray(img_array)  
-
-#cv2.imshow("decoded_image", dec_img)
-#pil_img.show()                                      
-pil_img.save("decoded_target15.jpg")
+if __name__ == "__main__":
+    img = cv2.imread("target15.jpg",0) #cv2.imreadで画像ファイルを開いてnumpy.ndarrayを取得
+    str = IMGtoBYTES(img)
+    BYTEStoIMG(str)
