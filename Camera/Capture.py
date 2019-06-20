e5d5f41aa30fbbe07d84e911b70940e837d966cc
unknown <s-arai14@jcom.zaq.ne.jp>
2019-06-20 16:14:46 +0900

    240,320

--- Camera/Capture.py
+++ Camera/Capture.py
@@ -5,7 +5,7 @@ def Capture(count):
     with picamera.PiCamera() as camera:
         camera.hflip = True
         camera.vflip = True
-        camera.resolution = (640,480)
+        camera.resolution = (240,320)
         #camera.start_preview()
         time.sleep(2)
         camera.capture('photo'+str(count)+'.jpg')
