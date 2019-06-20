5df6bffdec194c0f7b3bf70622be1495c01e4817
unknown <s-arai14@jcom.zaq.ne.jp>
2019-06-20 15:48:40 +0900

    ver2

--- IM920/test.py
+++ IM920/test.py
@@ -8,6 +8,7 @@ import binascii
 import IM920
 import convertIMG2BYTES
 import cv2
+
 '''
 pi=pigpio.pi()
 pi.set_mode(22,pigpio.OUTPUT)
@@ -15,23 +16,13 @@ pi.write(22,0)
 time.sleep(1)
 pi.write(22,0)
 '''
-print('a')
-IM920.Send('hello')
-print('b')
+
 
 
 img = cv2.imread("/home/pi/git/kimuralab/SensorModuleTest/Camera/photo1.jpg",0)
-bytes = convertIMG2BYTES.IMGtoBYTES(img)
+byte = convertIMG2BYTES.IMGtoBYTES(img)
 
-for i in range(0,len(bytes)):
-	st = str(bytes[i])
-	IM920.Send(st)
+for i in range(0,len(byte)):
+	IM920.IMSend(byte)
 
-
-
-#print(bytes)
-#st = str(bytes)
-#print (type(st))
-#print(st)
-#IM920.Send(st)
 #convertIMG2BYTES.BYTEStoIMG(st)
