51655606dfe21bd71d317dc459516a1e88751fa2
masatoshi_moto <45708319+masatoshiMoto@users.noreply.github.com>
2019-07-10 18:26:10 +0900

    Improve Center

--- BMX055/Calibration.py
+++ BMX055/Calibration.py
@@ -8,8 +8,6 @@ def ellipse(B, x):
     return ((x[0]/B[0])**2+(x[1]/B[1])**2-1.)
 
 def calibration(path):
-    #f = open("bmx055test.txt", "r")
-
     with open(path, "r") as f:
         lines = f.readlines()
         x_csv = []
@@ -19,18 +17,9 @@ def calibration(path):
             x_csv.append(float(word[0]))
             y_csv.append(float(word[1]))
 
-    '''
-    for i in range(len(x_csv)):
-        print(str(x_csv[i]) + " " + str(y_csv[i]))
-    print()
-    '''
-
     xx_csv = x_csv
     yy_csv = y_csv
 
-    # sub average
-    x_ave=np.average(x_csv)
-    y_ave=np.average(y_csv)
     n = len(x_csv)
     x2 = np.power(x_csv, 2)
     y2 = np.power(y_csv, 2)
@@ -41,19 +30,14 @@ def calibration(path):
 
     E = -(x_csv * (x2 + y2))
     F = -(y_csv * (x2 + y2))
-    H = -(x2+y2)
+    H = -(x2 + y2)
 
     x_csv = np.sum(x_csv)
     y_csv = np.sum(y_csv)
 
-    x_csv = x_csv-x_ave
-    y_csv = y_csv-y_ave
     x2 = np.sum(x2)
     y2 = np.sum(y2)
-
-    xy = np.array([x_csv, y_csv])
-    xy = np.sum(xy)    
-
+    
     E = np.sum(E)
     F = np.sum(F)
     H = np.sum(H)
@@ -73,14 +57,11 @@ def calibration(path):
     xy_csv = np.array([x_csv, y_csv])
 
     mdr = odr.Model(ellipse, implicit=True)
-    mydata = odr.Data(xy,y=1)
     mydata = odr.Data(xy_csv,y=1)
     myodr = odr.ODR(mydata, mdr, beta0=[1.,2.])
     myoutput = myodr.run()
 
     """from ellipse to Circle"""
-    x_csv = x_csv / 100.*myoutput.beta[1]
-    y_csv = y_csv / 100.*myoutput.beta[0]
     x_csv = x_csv / 100.* myoutput.beta[1]
     y_csv = y_csv / 100.* myoutput.beta[0]
 
@@ -95,7 +76,6 @@ def calibration(path):
 
 
 if __name__ == '__main__':
-    file = 'randomC.txt'
     file = 'cal_test_1.txt'
     cal_data = calibration(file)
     for i in range(4):
