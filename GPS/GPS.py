eb2f43497e7856caf25abdf08a6addb5e3512350
masatoshi_moto <45708319+masatoshiMoto@users.noreply.github.com>
2019-07-16 11:26:15 +0900

    20190716

--- GPS/GPS.py
+++ GPS/GPS.py
@@ -2,6 +2,7 @@ import sys
 import time
 import difflib
 import pigpio
+import numpy as np
 
 RX=26
 pi = pigpio.pi()
@@ -87,6 +88,26 @@ def closeGPS():
 	pi.bb_serial_read_close(RX)
 	pi.stop()
 
+def Cal_RhoAng(lat_a, lon_a, lat_b ,lon_b):
+	if(lat_a == lat_b and lon_a == lon_b):
+		return 0.0, 0.0
+	ra=6378.140  # equatorial radius (km)
+	rb=6356.755  # polar radius (km)
+	F=(ra-rb)/ra # flattening of the earth
+	rad_lat_a = np.radians(lat_a)
+	rad_lon_a = np.radians(lon_a)
+	rad_lat_b = np.radians(lat_b)
+	rad_lon_b = np.radians(lon_b)
+	pa = np.arctan(rb/ra*np.tan(rad_lat_a))
+	pb = np.arctan(rb/ra*np.tan(rad_lat_b))
+	xx = np.arccos(np.sin(pa)*np.sin(pb) + np.cos(pa)*np.cos(pb)*np.cos(rad_lon_a-rad_lon_b))
+	c1 = (np.sin(xx)-xx)*(np.sin(pa) + np.sin(pb))**2 / np.cos(xx/2)**2
+	c2 = (np.sin(xx)+xx)*(np.sin(pa) - np.sin(pb))**2 / np.sin(xx/2)**2
+	dr=F/8*(c1-c2)
+	rho=ra*(xx + dr) * 1000	#Convert To [m]
+	angle = math.atan2(lon_b-lon_a,  lat_b-lat_a)	#[rad]
+	return rho, angle
+	
 if __name__ == '__main__':
 	try:
 		openGPS()
