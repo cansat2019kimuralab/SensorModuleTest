602f0212962b3fd161a703134e27ffc949dcddfe
masatoshiMoto <k11000660@gmail.com>
2019-07-20 22:34:42 +0900

    20190720

--- Camera/Capture.py
+++ Camera/Capture.py
@@ -8,7 +8,7 @@ def Capture(count):
         camera.resolution = (320,240)	#(width,height)
         #camera.start_preview()
         time.sleep(2)
-        camera.capture('photo/photo'+str(count)+'.jpg')
+        camera.capture('/home/pi/photo/photo'+str(count)+'.jpg')
 
 if __name__ == "__main__":
 	Capture(1)
