5c90a230d15dc93258daa27a320484b23969975e
masatoshiMoto <k11000660@gmail.com>
2019-07-21 12:08:22 +0900

    20190721

--- Camera/Capture.py
+++ Camera/Capture.py
@@ -2,13 +2,14 @@ import time
 import picamera
 
 def Capture(count):
-    with picamera.PiCamera() as camera:
-        camera.hflip = True
-        camera.vflip = True
-        camera.resolution = (320,240)	#(width,height)
-        #camera.start_preview()
-        time.sleep(2)
-        camera.capture('/home/pi/photo/photo'+str(count)+'.jpg')
+	with picamera.PiCamera() as camera:
+		#camera.hflip = True
+		#camera.vflip = True
+		camera.rotation = 270
+		camera.resolution = (320,240)	#(width,height)
+		#camera.start_preview()
+		time.sleep(2)
+		camera.capture('/home/pi/photo/photo'+str(count)+'.jpg')
 
 if __name__ == "__main__":
 	Capture(1)
