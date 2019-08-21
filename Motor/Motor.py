0515d467efd0387a5050eb0433691ae143ca1463
arai <s-arai14@jcom.zaq.ne.jp>
2019-08-21 13:08:40 +0900

    change for debug

--- Motor/Motor.py
+++ Motor/Motor.py
@@ -113,20 +113,19 @@ def motor_stop():
 
 if __name__ == "__main__":
 	try:
-		#motor(70, 70, 1.5)
-
 		#motor(50, 0, 3)
 		#motor(0, 50, 3)
 		#motor(-50, 0, 3)
 		#motor(0, -50, 3)
-		motor(0, 0, 2, 0)
-		motor_stop()
+		#motor(0, 0, 2, 0)
+		#motor_stop()
 		while 1:
 			L = float(input("input left value "))
 			R = float(input("input Right value "))
 			T = float(input("input Time value "))
 			motor(L, R, T)
 			motor(0, 0, 2)
+			motor_stop()
 	except KeyboardInterrupt:
 		motor(0, 0, 3)
 		motor_stop()
